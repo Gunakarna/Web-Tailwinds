d490ef1d8e8e1a79396c2d0651456122f9c5de33
Gunakarna <97185726+Gunakarna@users.noreply.github.com>
2025-06-24 06:54:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",             // if you use Vite
+    "./src/**/*.{js,ts,jsx,tsx}" // this includes all JS/React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
